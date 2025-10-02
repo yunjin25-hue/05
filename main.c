d79db2ba82a2be861eb9ce59b266c80d6e2e8cf6
yunjin25-hue <yunjin25@sookmyung.ac.kr>
2025-10-02 15:35:54 +0900

    4

--- main.c
+++ main.c
@@ -4,17 +4,19 @@
 int main(int argc, char *argv[])
 {
     
-//실습 2
+//실습 4
   
   int x;
+  int i;
+  int sum=0;
   
-  printf("input an integer : ");
+  printf("input a number : ");
   scanf("%d",&x);
   
-  if(x>0)
-   printf("absolute is %d",x);
-  else if(x<0)
-   printf("absolute is %d",-x);
+  for(i=0;i<=x;i++)
+   sum=sum+i;              //조건보다 뭘 반복할지를 먼저 생각  
+  
+  printf("sum is %d\n",sum);
   
   system("PAUSE");	
   return 0;
